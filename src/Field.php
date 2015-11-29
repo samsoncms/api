79ec86883d68cf2e6d4df09ac2a0dbc3de0bb35a
Vitaly Iegorov <vitalyiegorov@gmail.com>
2015-11-30 00:36:49 +0200

    Added Field::byNameOrID() method

--- src/Field.php
+++ src/Field.php
@@ -1,6 +1,7 @@
 <?php
 namespace samsoncms\api;
 
+use samsonframework\orm\Condition;
 use samsonframework\orm\QueryInterface;
 
 /**
@@ -23,6 +24,29 @@ class Field extends \samson\activerecord\field
     {
         // Get field record by name column
         $return = static::oneByColumn($query, 'Name', $name);
+
+        // If only one argument is passed - return null, otherwise bool
+        return func_num_args() > 1 ? $return == null : $return;
+    }
+
+    /**
+     * Find additional field database record by Name or ID.
+     * This is generic method that should be used in nested classes to find its
+     * records by some its primary key value.
+     *
+     * @param QueryInterface $query Query object instance
+     * @param string $nameOrID Additional field name or identifier
+     * @param self $return Variable to return found database record
+     * @return bool|null|self  Field instance or null if 3rd parameter not passed
+     */
+    public static function byNameOrID(QueryInterface $query, $nameOrID, self & $return = null)
+    {
+        // Create id or URL condition
+        $idOrUrl = new Condition('OR');
+        $idOrUrl->add('FieldID', $nameOrID)->add('Name', $field);
+
+        // Perform query
+        $return = $query->className('field')->cond($idOrUrl)->first();
 
         // If only one argument is passed - return null, otherwise bool
         return func_num_args() > 1 ? $return == null : $return;
