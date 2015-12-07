b9efd4a8db9f75d71d2fc0cbaf57b7b4475c7bb8
Vitaly Iegorov <vitalyiegorov@gmail.com>
2015-12-07 12:19:51 +0200

    Changed system group name to Admin

--- sql/02_1.group.sql
+++ sql/02_1.group.sql
@@ -1,2 +1,2 @@
 INSERT INTO `@prefixgroup` (`GroupID`, `Name`, `Active`)
-VALUES ("2", "SamsonCMS", 1) ON DUPLICATE KEY UPDATE Active = 1;;
+VALUES ("2", "Admin", 1) ON DUPLICATE KEY UPDATE Active = 1;;
