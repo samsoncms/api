cd0c75a6694415cbe8833d17ef5b9d304f9e7768
Vitaly Iegorov <vitalyiegorov@gmail.com>
2015-12-24 13:21:24 +0200

    Removed generic user creation as it moved to samsoncms/signin

--- sql/05_2.user.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `@prefixuser` (`user_id`, `f_name`, `email`, `group_id`, `active`, `system`, `confirmed`)
-VALUES ("2", "Admin", 'admin@admin.com', 1, 1, 0, 1) ON DUPLICATE KEY UPDATE Active = 1;
