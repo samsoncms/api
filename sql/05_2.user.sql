8a64067cdc6680e89250c9a0ca4a83945ce45d3b
Vitaly Iegorov <vitalyiegorov@gmail.com>
2015-12-24 11:04:16 +0200

    Removed wrong columns for user credentials

--- sql/05_2.user.sql
+++ sql/05_2.user.sql
@@ -1,2 +1,2 @@
-INSERT INTO `@prefixuser` (`user_id`, `f_name`, `email`, `md5_email`, `md5_password`, `group_id`, `active`, `system`, `confirmed`)
-VALUES ("2", "Admin", 'admin@admin.com', '64e1b8d34f425d19e1ee2ea7236d3028', '64e1b8d34f425d19e1ee2ea7236d3028', 1, 1, 0, 1) ON DUPLICATE KEY UPDATE Active = 1;
+INSERT INTO `@prefixuser` (`user_id`, `f_name`, `email`, `group_id`, `active`, `system`, `confirmed`)
+VALUES ("2", "Admin", 'admin@admin.com', 1, 1, 0, 1) ON DUPLICATE KEY UPDATE Active = 1;
