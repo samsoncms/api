aa5b2db296aabf5cc8d81582993f9291bb36d272
Myslyvyi Dima <expenect@ukr.net>
2016-01-12 13:11:54 +0200

    added gallery class and fix bug's

--- src/Gallery.php
+++ src/Gallery.php
@@ -14,6 +14,7 @@ use samsonframework\orm\QueryInterface;
 /***
  *  Gallery additional field for material.
  *  This class enables getting all information about additional fields gallery for specific material.
+ *  @author myslyvyi@samsonos.com
  */
 class Gallery
 {
@@ -52,43 +53,80 @@ class Gallery
     }
 
     /**
-     * Check on empty gallery. If materialFieldId = 0 and quantity images not more 1 then material not has images.
+     * Check on empty gallery. If materialFieldId = null and quantity images not more 1 then material not has images.
      *
      * @return boolean
      **/
     public function hasImages()
     {
+        /**@var $hasImages */
+        $hasImages = false;
+
         if (isset($this->materialFieldId)) {
             // Getting quantity images, if quantity more 0 then material has images
             if ($this->query
             ->entity(CMS::MATERIAL_IMAGES_RELATION_ENTITY)
             ->cond(Field::F_DELETION, 1)
-            ->where(MaterialField::F_PRIMARY, $this->materialFieldId)
+            ->cond(MaterialField::F_PRIMARY, $this->materialFieldId)
             ->count() > 0) {
-                return true;
+                $hasImages = true;
             }
         }
-        return false;
+        return $hasImages;
+    }
+
+    /**
+     * Getting quantity images in additional field gallery
+     *
+     * @return integer $count
+     */
+    public function getCount()
+    {
+        /**@var integer $count quantity images in additional field gallery*/
+        $count = 0;
+
+        if ($this->hasImages()) {
+            // Getting quantity images for gallery
+            $count = $this->query
+                ->entity(CMS::MATERIAL_IMAGES_RELATION_ENTITY)
+                ->cond(Field::F_DELETION, 1)
+                ->cond(MaterialField::F_PRIMARY, $this->materialFieldId)
+                ->count();
+        }
+
+        return $count;
     }
 
     /**
      * Get collection of images for material by gallery additional field selector. If none is passed
      * all images from gallery table would be returned empty array.
      *
+     * @param integer $currentPage current page with images. Min value = 1
+     * @param integer $countView quantity view by page
      * @return array
      */
-    public function getImages()
+    public function getImages($currentPage = null, $countView = 20)
     {
         /** @var $images[] Get material images for this gallery */
         $images = array();
 
+        /** @var QueryInterface $query Database query interface*/
+        $query = null;
+
         if ($this->hasImages()) {
-            //Get All images for materialFieldId
-            $images = $this->query
+            // Select all images in DB by materialFieldId
+            $query = $this->query
                 ->entity(CMS::MATERIAL_IMAGES_RELATION_ENTITY)
                 ->cond(Field::F_DELETION, 1)
-                ->where(MaterialField::F_PRIMARY, $this->materialFieldId)
-                ->exec();
+                ->cond(MaterialField::F_PRIMARY, $this->materialFieldId);
+
+            if (isset($currentPage) && $currentPage > 0) {
+                //Set limit for query
+                $query->limit(--$currentPage * $countView, $countView);
+            }
+
+            // Execute query
+            $images = $query->exec();
         }
 
         return $images;
