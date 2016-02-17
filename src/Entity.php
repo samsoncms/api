bc2220311c0b366e7ba6212758939b76e189243d
Molodyko Ruslan <molodyko@samsonos.com>
2016-02-17 18:44:50 +0200

    Added saving entity logic

--- src/Entity.php
+++ src/Entity.php
@@ -19,4 +19,59 @@ use samsonframework\orm\QueryInterface;
 class Entity extends Material
 {
 
+    /** @var array Collection of navigation identifiers */
+    protected static $navigationIDs = array();
+
+    /** @var array Collection of localized additional fields identifiers */
+    protected static $fieldIDs = array();
+
+    /** @var array Collection of additional fields value column names */
+    protected static $fieldValueColumns = array();
+
+    /**
+     * Override default entity saving
+     */
+    public function save()
+    {
+        parent::save();
+
+        $relationEntity = CMS::MATERIAL_FIELD_RELATION_ENTITY;
+        foreach (static::$fieldIDs as $fieldID => $fieldName) {
+            /** @var \samson\activerecord\materialfield $materialfield */
+            $materialField = new $relationEntity();
+            $materialField->Active = 1;
+            $materialField->MaterialID = $this->id;
+            $materialField->FieldID = $fieldID;
+            $type = static::$fieldValueColumns[$fieldID];
+            $materialField->$type = $this->$fieldName;
+            $materialField->save();
+        }
+        $this->attachTo(static::$navigationIDs);
+    }
+
+    /**
+     * Add entity structure relation
+     * @param integer|array $structureID Structure identifier or their collection
+     */
+    public function attachTo($structureID)
+    {
+        $relationEntity = CMS::MATERIAL_NAVIGATION_RELATION_ENTITY;
+
+        foreach (is_array($structureID) ? $structureID : array($structureID) as $structureID) {
+            // Check if we do not have this relation already
+            if ($this->query
+                    ->entity($relationEntity)
+                    ->where(NavigationMaterial::F_PRIMARY, $structureID)
+                    ->where(self::F_PRIMARY, $this->id)
+                    ->count() === 0
+            ) {
+                /** @var \samson\activerecord\structurematerial $structureMaterial */
+                $structureMaterial = new $relationEntity();
+                $structureMaterial->Active = 1;
+                $structureMaterial->MaterialID = $this->id;
+                $structureMaterial->StructureID = $structureID;
+                $structureMaterial->save();
+            }
+        }
+    }
 }
--- src/generated/MaterialField.php
+++ src/generated/MaterialField.php
@@ -17,7 +17,26 @@ use samsonframework\orm\Record;
 if (!class_exists('\samson\activerecord\materialfield', false)) {
     class MaterialField extends Record
     {
+        /** @var integer Primary key */
+        public $FieldID;
 
+        /** @var bool Internal existence flag */
+        public $Active;
+
+        /** @var integer Material identifier */
+        public $MaterialID;
+
+        /** @var string Additional field value */
+        public $Value;
+
+        /** @var string Additional field value */
+        public $numeric_value;
+
+        /** @var string Additional field value */
+        public $key_value;
+
+        /** @var string Additional field locale */
+        public $locale;
     }
 }
 //[PHPCOMPRESSOR(remove,end)]
