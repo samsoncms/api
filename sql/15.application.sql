7cee4718953a89f981f555a19ea5076d462d557a
Olexandr Nazarenko <nazarenko@samsonos.com>
2016-02-19 17:39:22 +0200

    fixed sql queries

--- sql/15.application.sql
+++ sql/15.application.sql
@@ -1,7 +1,7 @@
 ALTER TABLE `structure` ADD `applicationGenerate` INT(1) NOT NULL DEFAULT '0' AFTER `hidden`;
-ALTER TABLE `field` ADD `applicationOutput` INT(1) NOT NULL DEFAULT '0' AFTER `applicationGenerate`;
-ALTER TABLE `field` ADD `applicationIcon` varchar(100) NOT NULL DEFAULT 'users' AFTER `applicationOutput`;
-ALTER TABLE `field` ADD `applicationRenderMain` INT(1) NOT NULL DEFAULT '1' AFTER `applicationIcon`;
+ALTER TABLE `structure` ADD `applicationOutput` INT(1) NOT NULL DEFAULT '0' AFTER `applicationGenerate`;
+ALTER TABLE `structure` ADD `applicationIcon` varchar(100) NOT NULL DEFAULT 'users' AFTER `applicationOutput`;
+ALTER TABLE `structure` ADD `applicationRenderMain` INT(1) NOT NULL DEFAULT '1' AFTER `applicationIcon`;
 ALTER TABLE `field` ADD `showInList` INT(1) NOT NULL DEFAULT '1' AFTER `system`;
 ALTER TABLE `field` ADD `showInForm` INT(1) NOT NULL DEFAULT '1' AFTER `showInList`;
 ALTER TABLE `field` ADD `customTypeName` VARCHAR(100) DEFAULT NULL AFTER `showInForm`;
