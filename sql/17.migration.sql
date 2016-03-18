45acf0ec7f9cf6e94be2e5d1f2db5ae0955f5612
getmanenko <getmanenko@samsonos.com>
2016-03-18 15:31:52 +0200

    Update migration sql

---
+++ sql/17.migration.sql
@@ -0,0 +1,17 @@
+ALTER TABLE `right` ADD `Description` VARCHAR(255) NOT NULL AFTER `Active`;
+ALTER TABLE `material` CHANGE `parent_id` `parent_id` INT(11) NULL DEFAULT NULL;
+ALTER TABLE `materialfield` CHANGE COLUMN `locale` `locale` VARCHAR(10) NULL DEFAULT NULL;
+UPDATE `materialfield` SET `locale` = NULL WHERE `locale` = '';
+Update `material` set parent_id = NULL WHERE parent_id = 0;
+ALTER TABLE 'material' DROP 'structure_id';
+ALTER TABLE `material` CHANGE `Created` `Created` DATETIME NOT NULL;
+ALTER TABLE `structure` CHANGE `MaterialID` `MaterialID` INT(11) NULL DEFAULT NULL;
+ALTER TABLE `structure` CHANGE `ParentID` `ParentID` INT(11) NULL DEFAULT NULL;
+update `structure` set ParentID = null WHERE ParentID = 0;
+ALTER TABLE `structure` CHANGE `UserID` `UserID` INT(11) NULL DEFAULT NULL;
+update `structure` set UserID = null WHERE UserID = 0;
+update `gallery` set Path = '/upload/' WHERE Path = '/cms/upload/';
+ALTER TABLE `field` CHANGE `ParentID` `ParentID` INT(11) NULL DEFAULT NULL;
+ALTER TABLE `field` CHANGE `UserID` `UserID` INT(11) NULL DEFAULT NULL;
+ALTER TABLE 'field' DROP 'ParentID';
+update `field` set UserID = null WHERE UserID = 0;
