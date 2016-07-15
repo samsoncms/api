dbac4fdcbf91ff195e8fa5d1c4c25e9da97e1ff0
vadimator <vpetrashenko@gmail.com>
2016-07-15 12:29:28 +0300

    Fixed migrations

--- sql/19.mysql57.sql
+++ sql/19.mysql57.sql
@@ -1,5 +1,4 @@
 ALTER TABLE `material` CHANGE `Draft` `Draft` INT(11) NOT NULL DEFAULT '0';
-ALTER TABLE `material` CHANGE `structure_id` `structure_id` INT(11) NOT NULL DEFAULT '0';
 ALTER TABLE `material` CHANGE `Created` `Created` DATETIME NULL DEFAULT NULL;
 ALTER TABLE `field` CHANGE `filtered` `filtered` INT(10) NOT NULL DEFAULT '0';
 ALTER TABLE `field` CHANGE `Active` `Active` INT(11) NOT NULL DEFAULT '0';
